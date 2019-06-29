6c4b6ed76d32cc212057a0f131cc390c0d70c958
rooom13 <romanreypadrero@gmail.com>
2019-06-29 12:24:04 +0200

    Updates

---
+++ precache-manifest.5db4007160d6b93848ee7d2c0c95652f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1481987ef90dffc707de2a77d630da70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62490be67afa557527a6",
+    "url": "/static/css/main.fee21a05.chunk.css"
+  },
+  {
+    "revision": "94af7f8b07d7d4877212",
+    "url": "/static/js/2.15fbe00d.chunk.js"
+  },
+  {
+    "revision": "62490be67afa557527a6",
+    "url": "/static/js/main.8ecbad45.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
