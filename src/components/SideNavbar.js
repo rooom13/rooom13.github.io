e6e71c82bae338a0b748623c019f6f077ecf6a8f
rooom13 <romanreypadrero@gmail.com>
2019-06-29 18:35:59 +0200

    clean

--- src/components/SideNavbar.js
+++ src/components/SideNavbar.js
@@ -1,44 +1,24 @@
 import React from 'react';
+import styled from 'styled-components'
+
 import '../App.css';
+import ProfilePic from '../images/profile.png'
 
 function SideNavbar() {
   return (
 
 
-    <nav id="sidebar" className="sidebar-wrapper">
+    <Sidebar id="sidebar" className="sidebar-wrapper">
       <div className="sidebar-content">
-        <div id="close-sidebar" className="sidebar-brand">
-          <a href="#">pro sidebar</a>
+        <div id="close-sidebar" >
+          <a href="#">-.- </a>
           <div >
             <i className="fas fa-times"></i>
           </div>
         </div>
         <div className="sidebar-header">
           <div className="user-pic">
-            <img className="img-responsive img-rounded" src="https://raw.githubusercontent.com/azouaoui-med/pro-sidebar-template/gh-pages/src/img/user.jpg"
-              alt="User picture" />
-          </div>
-          <div className="user-info">
-            <span className="user-name">Jhon
-            <strong>Smith</strong>
-            </span>
-            <span className="user-role">Administrator</span>
-            <span className="user-status">
-              <i className="fa fa-circle"></i>
-              <span>Online</span>
-            </span>
-          </div>
-        </div>
-        <div className="sidebar-search">
-          <div>
-            <div className="input-group">
-              <input type="text" className="form-control search-menu" placeholder="Search..." />
-              <div className="input-group-append">
-                <span className="input-group-text">
-                  <i className="fa fa-search" aria-hidden="true"></i>
-                </span>
-              </div>
-            </div>
+            <img className="img-responsive img-rounded" src={ProfilePic} />
           </div>
         </div>
         <div className="sidebar-menu">
@@ -48,156 +28,51 @@ function SideNavbar() {
             </li>
             <li className="sidebar-dropdown">
               <a href="#">
-                <i className="fa fa-tachometer-alt"></i>
-                <span>Dashboard</span>
-                <span className="badge badge-pill badge-warning">New</span>
+                <i className="fa "></i>
+                <span>CV</span>
               </a>
-              <div className="sidebar-submenu">
-                <ul>
-                  <li>
-                    <a href="#">Dashboard 1
-                    <span className="badge badge-pill badge-success">Pro</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">Dashboard 2</a>
-                  </li>
-                  <li>
-                    <a href="#">Dashboard 3</a>
-                  </li>
-                </ul>
-              </div>
             </li>
             <li className="sidebar-dropdown">
               <a href="#">
-                <i className="fa fa-shopping-cart"></i>
-                <span>E-commerce</span>
-                <span className="badge badge-pill badge-danger">3</span>
+                <i className="fa"></i>
+                <span>Projects  </span>
               </a>
               <div className="sidebar-submenu">
-                <ul>
-                  <li>
-                    <a href="#">Products
-
-                  </a>
-                  </li>
-                  <li>
-                    <a href="#">Orders</a>
-                  </li>
-                  <li>
-                    <a href="#">Credit cart</a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li className="sidebar-dropdown">
-              <a href="#">
-                <i className="far fa-gem"></i>
-                <span>Components</span>
-              </a>
-              <div className="sidebar-submenu">
-                <ul>
-                  <li>
-                    <a href="#">General</a>
-                  </li>
-                  <li>
-                    <a href="#">Panels</a>
-                  </li>
-                  <li>
-                    <a href="#">Tables</a>
-                  </li>
-                  <li>
-                    <a href="#">Icons</a>
-                  </li>
-                  <li>
-                    <a href="#">Forms</a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li className="sidebar-dropdown">
-              <a href="#">
-                <i className="fa fa-chart-line"></i>
-                <span>Charts</span>
-              </a>
-              <div className="sidebar-submenu">
-                <ul>
-                  <li>
-                    <a href="#">Pie chart</a>
-                  </li>
-                  <li>
-                    <a href="#">Line chart</a>
-                  </li>
-                  <li>
-                    <a href="#">Bar chart</a>
-                  </li>
-                  <li>
-                    <a href="#">Histogram</a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li className="sidebar-dropdown">
-              <a href="#">
-                <i className="fa fa-globe"></i>
-                <span>Maps</span>
-              </a>
-              <div className="sidebar-submenu">
-                <ul>
-                  <li>
-                    <a href="#">Google maps</a>
-                  </li>
-                  <li>
-                    <a href="#">Open street map</a>
-                  </li>
-                </ul>
               </div>
             </li>
             <li className="header-menu">
-              <span>Extra</span>
+              <span>Projects</span>
             </li>
             <li>
               <a href="#">
-                <i className="fa fa-book"></i>
-                <span>Documentation</span>
-                <span className="badge badge-pill badge-primary">Beta</span>
+                <i className="fa"></i>
+                <span>DailyWord bot</span>
               </a>
             </li>
             <li>
               <a href="#">
-                <i className="fa fa-calendar"></i>
-                <span>Calendar</span>
+                <i className="fa"></i>
+                <span>Project 2</span>
               </a>
             </li>
             <li>
               <a href="#">
-                <i className="fa fa-folder"></i>
-                <span>Examples</span>
+                <i className="fa"></i>
+                <span>Project 3</span>
               </a>
             </li>
           </ul>
         </div>
       </div>
-      <div className="sidebar-footer">
-        <a href="#">
-          <i className="fa fa-bell"></i>
-          <span className="badge badge-pill badge-warning notification">3</span>
-        </a>
-        <a href="#">
-          <i className="fa fa-envelope"></i>
-          <span className="badge badge-pill badge-success notification">7</span>
-        </a>
-        <a href="#">
-          <i className="fa fa-cog"></i>
-          <span className="badge-sonar"></span>
-        </a>
-        <a href="#">
-          <i className="fa fa-power-off"></i>
-        </a>
-      </div>
-    </nav>
+
+    </Sidebar>
 
   );
 }
+
+const Sidebar = styled.nav`
+  
+
+`
 
 export default SideNavbar;
