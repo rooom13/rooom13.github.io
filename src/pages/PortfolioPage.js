896eb2b99cbe1c3dca183b0ea1c15b2b3567d5a4
rooom13 <romanreypadrero@gmail.com>
2019-07-15 23:17:44 +0200

    timeline Desktop

--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components'
-// import media from '../media'
+import media from '../media'
 
 export default function () {
     return (
@@ -11,37 +11,37 @@ export default function () {
 
             <TimeLineWrapper>
             <Timeline >
-                <ContainerLeft class="left">
+                <ContainerLeft>
                     <Content >
                         <h2>2017</h2>
                         <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
                     </Content>
                 </ContainerLeft>
-                <ContainerRight class=" right">
+                <ContainerRight >
                     <Content >
                         <h2>2016</h2>
                         <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
                     </Content>
                 </ContainerRight>
-                <ContainerLeft class=" left">
+                <ContainerLeft >
                     <Content >
                         <h2>2015</h2>
                         <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
                     </Content>
                 </ContainerLeft>
-                <ContainerRight class=" right">
+                <ContainerRight >
                     <Content >
                         <h2>2012</h2>
                         <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
                     </Content>
                 </ContainerRight>
-                <ContainerLeft class=" left">
+                <ContainerLeft >
                     <Content >
                         <h2>2011</h2>
                         <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
                     </Content>
                 </ContainerLeft>
-                <ContainerRight class=" right">
+                <ContainerRight >
                     <Content >
                         <h2>2007</h2>
                         <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
@@ -51,45 +51,6 @@ export default function () {
             </TimeLineWrapper>
 
 
-
-            {/* <Timeline>
-                <ContainerLeft >
-                    <Content>
-                        <h2>2017</h2>
-                        <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
-                    </Content>
-                </ContainerLeft>
-                <ContainerRight >
-                    <Content>
-                        <h2>2016</h2>
-                        <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
-                    </Content>
-                </ContainerRight>
-                <ContainerLeft >
-                    <Content>
-                        <h2>2015</h2>
-                        <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
-                    </Content>
-                </ContainerLeft>
-                <ContainerRight >
-                    <Content>
-                        <h2>2012</h2>
-                        <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
-                    </Content>
-                </ContainerRight>
-                <ContainerLeft >
-                    <Content>
-                        <h2>2011</h2>
-                        <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
-                    </Content>
-                </ContainerLeft>
-                <ContainerRight >
-                    <Content>
-                        <h2>2007</h2>
-                        <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
-                    </Content>
-                </ContainerRight>
-            </Timeline> */}
 
 
 
@@ -120,23 +81,54 @@ const Container = styled.div`
     &:after {
         content: '';
         position: absolute;
-    width: 25px;
-    height: 25px;
-    right: -17px;
-    background-color: white;
-    border: 4px solid #FF9F55;
-    top: 15px;
-    border-radius: 50%;
-    z-index: 1;
+        width: 25px;
+        height: 25px;
+        right: -17px;
+        background-color: white;
+        border: 4px solid #FF9F55;
+        top: 15px;
+        border-radius: 50%;
+        z-index: 1;
+    }
 `
 const ContainerRight = styled(Container)`
-    // left: 50%;
-    // background-color: red;
+    left: 50%;
+
+    &::before {
+        content: " ";
+        height: 0;
+        position: absolute;
+        top: 22px;
+        width: 0;
+        /* z-index: 1; */
+        left: 30px;
+        border: medium solid white;
+        border-width: 10px 10px 10px 0;
+        border-color: transparent white transparent transparent;
+    }
+
+    &:after {
+        left: -16px;
+
+    }
 `
 
 const ContainerLeft = styled(Container)`
-    // left: 0%;
-    // background-color: blue;
+    left: 0%;
+    
+    &:before {
+        
+        content: " ";
+        height: 0;
+        position: absolute;
+        top: 22px;
+        width: 0;
+        /* z-index: 1; */
+        right: 30px;
+        border: medium solid white;
+        border-width: 10px 0 10px 10px;
+        border-color: transparent transparent transparent white;
+    }
 
 `
 
@@ -147,14 +139,20 @@ const Timeline = styled.div`
     margin: 0 auto;
 
     &:after {
-        content: '';
-        position: absolute;
-        width: 6px;
-        background-color: white;
-        top: 0;
-        bottom: 0;
-        left: 50%;
-        margin-left: -3px;
+    
+    content: '';
+    position: absolute;
+    width: 6px;
+    background-color: white;
+    top: 0;
+    bottom: 0;
+    left: 50%;
+    margin-left: -3px;
+    
+    // ${media.tablet`
+    //     left: 50%;
+    // `}
+
     }
 `
 
