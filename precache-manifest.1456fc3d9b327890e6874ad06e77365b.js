091243a2b44986eeb4472a626835bae6aacc4517
rooom13 <romanreypadrero@gmail.com>
2019-07-29 23:00:30 +0200

    Updates

--- precache-manifest.1456fc3d9b327890e6874ad06e77365b.js
+++ precache-manifest.1456fc3d9b327890e6874ad06e77365b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c094a3e021e7b135bf3afff17ef4215d",
+    "revision": "11a3c1721603fd03f90b435acb58d631",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.8a31395d.chunk.js"
   },
   {
-    "revision": "62718399654ef5125a1b",
-    "url": "/static/js/main.678e7e39.chunk.js"
+    "revision": "0965aae866394389ca6b",
+    "url": "/static/js/main.30401ce1.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "5b8021e16b936a2082a953a2bff8f721",
-    "url": "/static/media/CV_RomanRey_en.5b8021e1.pdf"
+    "revision": "2ea6d9a909b7c2d23374f96dd87286ab",
+    "url": "/static/media/CV_RomanRey_en.2ea6d9a9.pdf"
   },
   {
     "revision": "9ddc953d22e223ea1b7c9b58b5407979",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66fdfd7ff9c0717b2cb40a5ccc345677.js"
+  "/precache-manifest.1456fc3d9b327890e6874ad06e77365b.js"
 );
 
 self.addEventListener('message', (event) => {
