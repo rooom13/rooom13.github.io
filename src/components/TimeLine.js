9e38bbaabf00f74214aed31a349347a5959ca6dd
Roman <roman.rey@hawk.ai>
2022-01-15 17:00:24 +0100

    added thumbnails

--- src/components/TimeLine.js
+++ src/components/TimeLine.js
@@ -3,6 +3,9 @@ import styled from 'styled-components'
 import media from '../media'
 import { FONTCOLOR, SECONDARYCOLOR } from '../Colors';
 import Link from './Link'
+import mathicRunnerPicPath from '../images/mathic-runner.jpg'
+import mathicPlatformerPicPath from '../images/mathic-platformer.jpg'
+import samplesLooperPicPath from '../images/samples-looper.jpg'
 
 export default function () {
 
@@ -21,40 +24,37 @@ export default function () {
         <Timeline >
             <ContainerLeft >
                 <Content >
-                    <h2><Link href={topTweetsBotLink}> @hot_tweets_bot</Link> | 2021 - {new Date().getFullYear()}</h2>
+                    <h2><Link target="blank" href={topTweetsBotLink}> @top_tweets_bot</Link> | 2021 - {new Date().getFullYear()}</h2>
                     <div>
-                        Investment psychology in a connected society can be reflected on people's most popular thoughts. hot_tweets_bot provides the top tweets of topics of your choice to make you stay up to date with the latest announcements.
-                        <p>
-                            The bot connects to the Twitter API to retrieve the most significant tweets.</p>
+                        Investment psychology in a connected society can be reflected on people's most popular thoughts. @top_tweets_bot provides the top tweets of topics of your choice to make you stay up to date with the latest announcements.
+                        <p>The bot connects to the Twitter API to retrieve the most significant tweets.</p>
                     </div>
                 </Content>
             </ContainerLeft>
             <ContainerRight >
                 <Content >
-                    <h2><Link href={samplesLooperLink}> Samples Looper</Link> | 2020 </h2>
+                    <h2><Link target="blank" href={samplesLooperLink}> Samples Looper</Link> | 2020 </h2>
                     <div>
-                        I wanted to experiment with the Web Audio API and what best to develop a web app for mixing samples.
+                        I wanted to experiment with the Web Audio API so I developed a web app for mixing samples.
                         The app allows you to load a set of samples and play them along.
-                        <p>
-                            The app can be controlled with the keyboard since mixing needs changing settings quite fast, which might not be so comfortable using the cursor.
-                        </p>
+                        <a target="blank" href={samplesLooperLink}><StyledImage src={samplesLooperPicPath} /></a>
+                        <p>The app can be controlled with the keyboard since mixing needs changing settings quite fast, which might not be so comfortable using the cursor.</p>
                     </div>
                 </Content>
             </ContainerRight>
             <ContainerLeft >
                 <Content >
-                    <h2><Link href={dailyWordBotLink}> @dailyWord_bot</Link> | 2018 - {new Date().getFullYear()}</h2>
+                    <h2><Link target="blank" href={dailyWordBotLink}> @dailyWord_bot</Link> | 2018 - {new Date().getFullYear()}</h2>
                     <div>
                         Telegram Bot to help expanding Germand/Spanish vocabulary.
-                        <p>
-                            Developed using <b>Python</b> and <b>Redis</b> & deployed using <b>Cloud Computing</b> techniques with <b>Docker</b> as a container platform.</p>
+                        <p>Developed using <b>Python</b> and <b>Redis</b> & deployed using <b>Cloud Computing</b> techniques with <b>Docker</b> as a container platform.</p>
                     </div>
 
                 </Content>
             </ContainerLeft>
             <ContainerRight >
                 <Content >
-                    <h2><Link href={thesisLink} >Recommendation Systems Thesis</Link> | 2019</h2>
+                    <h2><Link target="blank" href={thesisLink} >Recommendation Systems Thesis</Link> | 2019</h2>
                     <div>
                         Evaluation of a <b>collaborative filtering</b> method, <b>conten-based</b> method and hybrid approach applied to musical artist recommendation.
                     </div>
@@ -62,9 +62,8 @@ export default function () {
             </ContainerRight>
             <ContainerLeft >
                 <Content >
-                    <h2><Link href={visualsLink}>"In the Universe" visuals</Link> | 2018</h2>
-                    <p> Lyric visuals for the videoclip of <i>"In the Universe"</i> song. The visuals were developed using Javascript's canvas with the challenge of synchronizing the audio, lyrics and visual effects.
-                    </p>
+                    <h2><Link target="blank" href={visualsLink}>"In the Universe" visuals</Link> | 2018</h2>
+                    <p> Lyric visuals for the videoclip of <i>"In the Universe"</i> song. The visuals were developed using Javascript's canvas with the challenge of synchronizing the audio, lyrics and visual effects.</p>
                     <StyledIframe
                         src={'https://www.youtube.com/embed/jMQ0V9Dbbyg'}
                         frameborder="0"
@@ -75,20 +74,21 @@ export default function () {
             </ContainerLeft>
             <ContainerRight >
                 <Content >
-                    <h2> <Link href={runnerLink}>Mathic Runner</Link> | 2014</h2>
+                    <h2> <Link target="blank" href={runnerLink}>Mathic Runner</Link> | 2014</h2>
                     <p>Web responsive videogame developed for <i>genmagic.net</i>an educational platform of research and development of interactive applications.</p>
+                    <a target="blank" href={runnerLink}><StyledImage src={mathicRunnerPicPath} /></a>
                 </Content>
             </ContainerRight>
             <ContainerLeft >
                 <Content >
-                    <h2><Link href={platformerLink}>Mathic Platformer</Link> | 2013</h2>
-                    <p>Web app videogame for a research project that approaches education and gamification.
-                    </p>
+                    <h2><Link target="blank" href={platformerLink}>Mathic Platformer</Link> | 2013</h2>
+                    <p>Web app videogame for a research project that approaches education and gamification.</p>
+                    <a href={platformerLink}><StyledImage src={mathicPlatformerPicPath} /></a >
                 </Content>
             </ContainerLeft>
             <ContainerRight >
                 <Content >
-                    <h2><Link href={animationsLink}>Animations</Link> | Long time ago</h2>
+                    <h2><Link target="blank" href={animationsLink}>Animations</Link> | Long time ago</h2>
                     <p> At an early age I spent my time doing animations using <b>Adobe Flash</b>.
                         <StyledIframe
                             src="https://www.youtube.com/embed/videoseries?list=PLZIKq90MlT8IbjRKOPOjL2AZSRRrWApLI"
@@ -102,6 +102,11 @@ export default function () {
         </Timeline >
     )
 }
+
+const StyledImage = styled.img`
+    border-radius: 5px;
+    width: 100%;
+`
 
 
 const StyledIframe = styled.iframe`
@@ -135,7 +140,7 @@ const Container = styled.div`
     ${media.tablet`
         margin-top: 0;
         width: 50%;
-        padding: 0.6rem 2.5rem;
+        padding: 0.6rem 0rem;
     `}
 
     &:after {
@@ -176,6 +181,10 @@ const ContainerRight = styled(Container)`
         &:after {
             left: -16px; 
         }
+
+        > div {
+            margin-left: 2.5rem;
+        }
     `}
 `
 
@@ -198,9 +207,10 @@ const ContainerLeft = styled(Container)`
     }
     
     ${media.tablet`
-
-        > div {
-            border-left: none;
+    
+    > div {
+        margin-right: 2.5rem;
+        border-left: none;
             border-right: 1px solid;
         }
         &:before {
